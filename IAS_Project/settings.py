1af126656cc7e85a3016cc88b6355563c256ef13
Priyanshu Arora <priyanshuarora595@gmail.com>
2022-08-25 16:00:36 +0530

    deployment settings

--- IAS_Project/settings.py
+++ IAS_Project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-5)!lwh*)zvf&1amx*5jgb79tr07b6-*3f6^t#27r=et!v&^8fw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','udaipuriams.herokuapp.com']
 
 
 # Application definition
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
